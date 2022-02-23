3dce34e0d9c0deb41a1bc45664eeca323af09d6b
dyc <dycyachao0818@163.com>
2022-02-23 20:11:17 +0800

    add demos

---
+++ src/Formily.vue
@@ -0,0 +1,39 @@
+<template>
+  <Form form="{form}" labelCol="{6}" wrapperCol="{10}">
+    <SchemaField
+      :schema="{
+        type: 'object',
+        properties: {
+          input: {
+            'title': '最大值(>5报错)',
+            'type': 'string',
+            'x-component': 'Input',
+          },
+        },
+      }"
+    >
+    </SchemaField>
+  </Form>
+</template>
+
+<script>
+import { Input, Form } from 'ant-design-vue';
+import { createForm } from '@formily/core';
+import { FormProvider, createSchemaField } from '@formily/vue';
+import 'ant-design-vue/dist/antd.css';
+
+const { SchemaField } = createSchemaField({
+  components: {
+    Input,
+  },
+});
+
+export default {
+  components: { FormProvider, SchemaField },
+  data() {
+    return {
+      form: createForm(),
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-const app = createApp(App);
-app.mount('#app');
-console.log('app', app);
+// import App from './App.vue';
+// const app = createApp(App);
+// app.mount('#app');
+import Formily from './Formily.vue';
+const FormilyApp = createApp(Formily);
+FormilyApp.mount('#formily');
